dc749f4cc199b0cc8ba63b815fc9da9f1a453355
Cerek Hillen <cerekh@gmail.com>
2023-09-10 22:04:58 -0700

    implement a database

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod database;
+
+use crate::database::MemoryDatabase;
+
+static mut DATABASE: Option<MemoryDatabase> = None;
 
 fn main() {
+    unsafe {
+        // This runs first, and is the only place that we touch `DATABASE` mutably.
+        // I'm sure that there's a better way, but I'll deal with it later.
+        DATABASE.replace(database::MemoryDatabase::new());
+    }
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
