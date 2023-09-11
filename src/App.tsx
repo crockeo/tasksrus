3100eb111047eddf4fcec929307a1673ce32cbaf
Cerek Hillen <cerekh@gmail.com>
2023-09-10 22:38:33 -0700

    indentation??

--- src/App.tsx
+++ src/App.tsx
@@ -21,51 +21,51 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+	    <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
+	    <div className="row">
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+		      <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
         <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+		      <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+		  <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+	    </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+	    <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-      <form
+	    <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
           greet();
         }}
-      >
+	    >
         <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+		      id="greet-input"
+		      onChange={(e) => setName(e.currentTarget.value)}
+		      placeholder="Enter a name..."
         />
         <button type="submit">Greet</button>
-      </form>
+	    </form>
 
-      <p>{greetMsg}</p>
+	    <p>{greetMsg}</p>
 
-      <button onClick={(e) => {
-	e.preventDefault();
-	getTasks();
-      }}>Get Tasks</button>
+	    <button onClick={(e) => {
+	      e.preventDefault();
+	      getTasks();
+	    }}>Get Tasks</button>
 
-      <div>
-	  {tasks.map((task, i) =>
 	    <div>
-		{task.inner_id} {task.title}
+	      {tasks.map((task, i) =>
+		      <div>
+		        {task.inner_id} {task.title}
+		      </div>
+	      )}
 	    </div>
-	  )}
-      </div>
 
     </div>
   );
