862ffb8fda9be9971365c6ae13e12f47d3533600
Cerek Hillen <cerekh@gmail.com>
2023-09-11 18:37:21 -0700

    some more formatting and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,30 @@ interface Task {
   title: string
 }
 
+enum Mode {
+  Inbox = "Inbox",
+  Today = "Today",
+  Upcoming = "Upcoming",
+  Anytime = "Anytime",
+  Someday = "Someday",
+  Logbook = "Logbook",
+  Trash = "Trash",
+}
+
+type View = Mode | string;
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
-  const [currentTask, setCurrentTask] = useState(0);
+  const [currentView, setCurrentView] = useState(Mode.Inbox);
   const [tasks, setTasks] = useState([
     {
-      inner_id: 0,
+      inner_id: 12341,
       title: "Work on tasksrus",
     },
     {
-      inner_id: 1,
+      inner_id: 518283,
       title: "Also do my job lol",
     },
   ]);
@@ -40,35 +52,40 @@ function App() {
     <div className="container">
       <div className="task-list">
 	<div>
-	  <div className="title">Tasks</div>
+          <div className="title">Tasks</div>
 
-	  <div className="space-medium"></div>
+          <div className="space-medium"></div>
 
-	  <div className="subtitle">Inbox</div>
+	  <Category mode={Mode.Inbox} currentView={currentView} setCurrentView={setCurrentView} />
 
-	  <div className="space-small"></div>
+          <div className="space-small"></div>
 
-	  <div className="subtitle">Upcoming</div>
-	  <div className="subtitle">Anytime</div>
-	  <div className="subtitle">Someday</div>
+	  <Category mode={Mode.Today} currentView={currentView} setCurrentView={setCurrentView} />
+	  <Category mode={Mode.Upcoming} currentView={currentView} setCurrentView={setCurrentView} />
+	  <Category mode={Mode.Anytime} currentView={currentView} setCurrentView={setCurrentView} />
+	  <Category mode={Mode.Someday} currentView={currentView} setCurrentView={setCurrentView} />
 
-	  <div className="space-small"></div>
+          <div className="space-small"></div>
 
-	  <div className="subtitle">Logbook</div>
-	  <div className="subtitle">Trash</div>
+	  <Category mode={Mode.Logbook} currentView={currentView} setCurrentView={setCurrentView} />
+	  <Category mode={Mode.Trash} currentView={currentView} setCurrentView={setCurrentView} />
 
-	  <div className="space-medium"></div>
+          <div className="space-medium"></div>
 
-	  <div>
-	    {tasks.map((task) =>
-	      <Task key={task.inner_id} selected={currentTask == task.inner_id} task={task} />
-	    )}
-	  </div>
+          <div>
+            {tasks.map((task) =>
+              <Task
+		key={task.inner_id}
+		task={task}
+		currentView={currentView}
+		setCurrentView={setCurrentView}
+	      />
+            )}
+          </div>
 	</div>
 
-
 	<div>
-	  +
+          +
 	</div>
       </div>
 
@@ -79,18 +96,48 @@ function App() {
   );
 }
 
+interface ICategoryProps {
+  mode: Mode,
+  currentView: View,
+  setCurrentView: (View) => void,
+}
+
+function Category(props: ICategoryProps) {
+  return (
+    <div
+      className={
+	classNames("category", "task", {
+	  "task-selected": props.currentView == props.mode
+	})
+      }
+      onClick={(_) => props.setCurrentView(props.mode)}
+    >
+      {props.mode}
+    </div>
+  );
+}
+
 interface ITaskProps {
-  selected: bool,
   task: Task,
+  currentView: View,
+  setCurrentView: (View) => void,
 }
 
 function Task(props: ITaskProps) {
   // TODO: this text is not 100% centered in its block.
   // why is that? do we have to tune something about where the text sits relative to its "line"?
   return (
-    <div className={classNames("task", {
-      "task-selected": props.selected,
-    })}>
+    <div
+      className={
+	classNames("task", {
+	  "task-selected": props.currentView == props.task.inner_id,
+	})
+      }
+      onClick={(_) => {
+	console.log(props.currentView);
+	props.setCurrentView(props.task.inner_id)
+      }}
+    >
       {props.task.title}
     </div>
   );
